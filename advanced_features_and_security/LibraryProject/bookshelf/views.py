a74bd6328227cec4e8881ff02e9ddfdfdc7f3bca
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-10-12 22:48:18 +0000

    managing permissions ang groups 3

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# 👇 The view required by the checker
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    # Retrieve all books from the database
+    books = Book.objects.all()
+    # Render the books list template
+    return render(request, 'bookshelf/book_list.html', {'books': books})

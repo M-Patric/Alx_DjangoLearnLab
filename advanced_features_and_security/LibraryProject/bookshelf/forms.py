3ed0936165ef268cbc0295548da412b36fc8a4b6
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-10-14 14:26:58 +0000

    Implementing security best practices in Django 2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
-class BookSearchForm(forms.Form):
-    q = forms.CharField(max_length=200, required=False)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Your Name")
+    email = forms.EmailField(label="Your Email")
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 4, "placeholder": "Type your message here..."}),
+        label="Message",
+    )
